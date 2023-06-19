b52ffe69db0d2334c80ad8491279cedd0e650a53
patrick smith <smithpatrick.510@gmail.com>
2023-06-19 12:55:24 -0400

    methods exercise

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        { 
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        { 
+            return a * b; 
+        }
+
+        public static double Divide(double a, double b)
+        { 
+            return a / b; 
+        }
+
+
+       
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace MethodsExercise
+﻿using System.Threading.Channels;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int product = Methods.Multiply(7, 9);
+            Console.WriteLine(product);
+            double quotient = Methods.Divide(7, 9);
+            Console.WriteLine(quotient);
+            int difference = Methods.Subtract(7,9);
+            Console.WriteLine(difference);
+            int sum = Methods.Add(7,9);
+            Console.WriteLine(sum);
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine(); // input
+            Console.WriteLine("What is your favorite color");
+            var color = Console.ReadLine(); //input
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine(); //input
+            Console.WriteLine($"Hello. My name is {name} and I love the color {color}, especially on my favorite animal, the {animal}");
         }
-    }
+
+
+            }
 }
